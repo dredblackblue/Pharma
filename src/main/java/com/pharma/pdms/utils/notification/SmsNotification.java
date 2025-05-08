b031e5f6430f5281dcf6c7864e1b4919e8ea8665
dredblackblue <PES2UG22CS171@pesu.pes.edu>
2025-05-08 12:00:53 +0530

    Initial commit

--- src/main/java/com/pharma/pdms/utils/notification/SmsNotification.java
+++ src/main/java/com/pharma/pdms/utils/notification/SmsNotification.java
@@ -3,17 +3,15 @@ package com.pharma.pdms.utils.notification;
 import org.springframework.stereotype.Component;
 
 /**
- * SMS notification implementation
- * Part of the Factory Design Pattern
+ * SMS implementation of Notification interface
  */
 @Component
-public class SmsNotification implements Notification {
+public class SMSNotification implements Notification {
     @Override
-    public void send(String recipient, String subject, String message) {
-        // In a real implementation, this would use an SMS gateway like Twilio
-        System.out.println("Sending SMS notification:");
-        System.out.println("To: " + recipient);
-        System.out.println("Message: " + message); // SMS typically doesn't have a subject
-        System.out.println("----------------------------------");
+    public void send(String recipient, String subject, String content) {
+        // Implementation for sending SMS
+        System.out.println("Sending SMS notification to: " + recipient);
+        System.out.println("Message: " + subject + " - " + content);
+        // In a real implementation, this would use an SMS service provider API
     }
 }
